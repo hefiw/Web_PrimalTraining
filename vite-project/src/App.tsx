4510200a9df283225d7b3d0b7ae26efb56caef51
hefiw <110940256+hefiw@users.noreply.github.com>
2025-10-10 22:32:47 +0300

    v2

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "../src/styles/main.css";
+//import "../src/styles/main.css";
+import '../../css/main.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { routes } from "./routes/routes";
 
